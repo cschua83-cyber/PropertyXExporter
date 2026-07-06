23dea01b10bfc8d2b7f54e8ba7de25c16501dd7f
cs chua <cschua83@gmail.com>
2026-07-06 13:58:34 +0800

    Add live sales and block navigation

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from src.browser import get_browser
+from src.navigation import open_live_sales, get_block_tabs
 
 browser = get_browser()
 
 driver = browser.get_driver()
 
-print(driver.title)
-print(driver.current_url)
+browser = get_browser()
+
+driver = browser.get_driver()
+
+if "unit-live" not in driver.current_url:
+    open_live_sales(driver)
+
+tabs = get_block_tabs(driver)
+
+block_tabs = tabs[1:]   # 跳过 PHASE
+
+print()
+print("Blocks")
+print("-" * 40)
+
+for tab in block_tabs:
+    print(tab.text)
 
 input("Press Enter...")
--- src/navigation.py
+++ src/navigation.py
@@ -9,15 +9,86 @@ navigation.py
 """
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def debug_tabs(driver):
+
+    print("=" * 60)
+    print("Searching Tabs...")
+    print("=" * 60)
+
+    elements = driver.find_elements(
+        By.CSS_SELECTOR,
+        "[role='tab']"
+    )
+
+    print(f"Found {len(elements)} elements")
+
+    for i, e in enumerate(elements):
+
+        print("-" * 40)
+        print(f"Tab {i}")
+        print("Text :", e.text)
+        print("Class:", e.get_attribute("class"))
+        
+
+def open_live_sales(driver):
+
+    print("=" * 60)
+    print("Opening Live Sales...")
+    print("=" * 60)
+
+    # 记录目前有几个 Tab
+    old_tab_count = len(driver.window_handles)
+
+    # 找到 Live Sales Chart 按钮
+    button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//button[.//span[contains(text(),'Live Sales Chart')]]"
+            )
+        )
+    )
+
+    # 点击按钮
+    button.click()
+
+    print("Live Sales Chart clicked.")
+
+    # 等待新 Tab 开启
+    WebDriverWait(driver, 10).until(
+        lambda d: len(d.window_handles) > old_tab_count
+    )
+
+    # 切换到最新 Tab
+    driver.switch_to.window(driver.window_handles[-1])
+
+    print("Switched to Live Sales")
+
+    print(driver.title)
+    print(driver.current_url)
+
+    print("=" * 60)
+    
 
 def get_block_tabs(driver):
+
+    WebDriverWait(driver, 10).until(
+        lambda d: len(
+            d.find_elements(By.CSS_SELECTOR, "div[role='tab']")
+        ) >= 4
+    )
+
     tabs = driver.find_elements(
         By.CSS_SELECTOR,
         "div[role='tab']"
     )
 
     print("=" * 60)
-    print(f"Found {len(tabs)} Block Tabs")
+    print(f"Found {len(tabs)} Tabs")
     print("=" * 60)
 
     for i, tab in enumerate(tabs):
